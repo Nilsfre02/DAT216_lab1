e7cfeed3d5fbc8d3cfe9402bc442926d60fb534d
Oliver Isaksson <oliverisaksson@proton.me>
2026-03-25 16:29:56 +0100

    replace template

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/model/task_handler.dart';
+import 'package:lab1/pages/add_view.dart';
+import 'package:lab1/widgets/task_list.dart';
+import 'package:provider/provider.dart';
+import 'package:lab1/model/types.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -6,15 +11,45 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('DAT216/TIG091'),
-            Text('Lab1 Todos'),
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(
+            title: const Text('Things todo'),
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            actions: _actions(context)),
+        body: const TaskList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddView(),
+              ),
+            );
+          },
+          tooltip: 'Add todo',
+          child: const Icon(Icons.add),
+        )); // Scaffold
   }
+}
+
+List<Widget> _actions(context) {
+  return [_filterMenu(context)];
+}
+
+Widget _filterMenu(context) {
+  var taskHandler = Provider.of<TaskHandler>(context, listen: false);
+
+  return PopupMenuButton<FilterType>(
+    initialValue: taskHandler.mode,
+    onSelected: (FilterType item) {
+      taskHandler.setMode(item);
+    },
+    itemBuilder: (BuildContext context) => [
+      const PopupMenuItem<FilterType>(
+          value: FilterType.all, child: Text('All')),
+      const PopupMenuItem<FilterType>(
+          value: FilterType.done, child: Text('Done')),
+      const PopupMenuItem<FilterType>(
+          value: FilterType.undone, child: Text('Undone')),
+    ],
+  );
 }
